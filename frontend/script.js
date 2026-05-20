725a164169051e28c31072e69042a1d2a7c8ae5d
Gao Yang <lokiray4camus@gmail.com>
2026-05-20 22:26:42 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,38 @@
+async function upload(){
 
+const file=
+document.getElementById(
+"video"
+).files[0]
+
+const formData=
+new FormData()
+
+formData.append(
+"file",
+file
+)
+
+const response=
+await fetch(
+"后面替换",
+{
+method:"POST",
+body:formData
+}
+)
+
+const data=
+await response.json()
+
+document.getElementById(
+"result"
+).innerHTML=
+`
+<h3>丹麦语</h3>
+${data.danish}
+
+<h3>中文</h3>
+${data.translation}
+`
+}
